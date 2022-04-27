da2dcffc07626cd154119a9e579968b922689987
Braden <braden@bradenn.com>
2022-04-27 14:01:22 -0700

    Implemented allnighter function. Reverted other functions to a working state after building tests to cover input variations. Added multiple tests for each function in Practice.cpp.

--- Practice.cpp
+++ Practice.cpp
@@ -6,27 +6,23 @@ using std::string;
 // Receive three integers and rearrange their values so that they are in
 // descending order from greatest (first) to least (third)
 void Practice::sortDescending(int &first, int &second, int &third) {
-    int biggest, middle, smallest;
-    if (second > third && second > first) {
-        biggest = second;
-    } else if (third > second && third > first) {
-        biggest = third;
-    } else {
-        biggest = first;
+    if( first < second )
+    {
+        int temp = first;
+        first = second;
+        second = temp;
     }
-    if (second < third && second < first) {
-        smallest = second;
-    } else if (third < second && third < first) {
-        smallest = third;
-    } else {
-        smallest = first;
+    if( first < third )
+    {
+        int temp = first;
+        first = third;
+        third = temp;
     }
-    if (first != biggest && first != smallest) {
-        middle = first;
-    } else if (second != biggest && second != smallest) {
-        middle = second;
-    } else {
-        middle = third;
+    if( second < third )
+    {
+        int temp = second;
+        second = third;
+        third = temp;
     }
 }
 
@@ -34,43 +30,39 @@ void Practice::sortDescending(int &first, int &second, int &third) {
 // where it is spelled the same backwards and forwards when considering every
 // character in the string, but disregarding case ('x' is the same as 'X')
 bool Practice::isPalindrome(string input) {
-    for (unsigned int i = 0; i < input.size(); i++) {
-        if (input[i] < 'A' || input[i] > 'Z') {
+    for(unsigned int i=0; i < input.size(); i++)
+    {
+        if( input[i] < 'A' || input[i] > 'Z' )
+        {
             //change lower case to upper case
             input[i] = input[i] - ('a' - 'A');
         }
     }
-
-    bool match = false;
-
-    for (unsigned int i = 0; i < input.size() / 2; i++) {
-        if (input[i] == input[input.size() - 1 - i])
-            match = true;
-        else
-            match = false;
+    for(unsigned int i=0; i < input.size()/2; i++)
+    {
+        if( input[i] != input[input.size()-1-i] )
+            return false;
     }
-    return match;
+    return true;
 }
 
 // This function receives a string and counts how many times the same character
 // is repeated at the beginning of the string, before any other characters. The
 // function is case sensative so 'Z' is different than 'z'.
 int Practice::count_starting_repeats(string word) {
-    int repetition = 0;
+    if(word == "") return 0;
+    int repetition = 1;
     int index = 0;
-    char letter;
 
-    if (word.length() > 0)
-        letter = word[0];
-
-    for (unsigned int i = 1; i < word.length(); i++) {
-        if (word[i] == letter) {
-            repetition++;
-        }
+    while( index < word.length()-1 && word[index] == word[index+1] ){
+        repetition++;
+        index++;
     }
 
     return repetition;
 }
+
+
 
 
 // Receives an array that represents the hours someone sleeps each night of the week
--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -19,92 +19,167 @@ protected:
 
     int *allnighter(int sleep[7]);
 };
-//
-//TEST(PracticeTest, smoke_test)
-//{
-//ASSERT_TRUE(true);
-//}
-//
-//TEST(PracticeTest, countStartingRepeatsTest1) {
-//Practice testObj;
-//ASSERT_EQ(testObj.count_starting_repeats("zzzZzzzzz"), 3);
-//}
-//
-//TEST(PracticeTest, countStartingRepeatsTest2) {
-//Practice testObj;
-//ASSERT_EQ(testObj.count_starting_repeats("ppppppppppp"), 11);
-//}
-//
-//TEST(PracticeTest, countStartingRepeatsTest3) {
-//Practice testObj;
-//ASSERT_EQ(testObj.count_starting_repeats(""), 0);
-//}
-//
-//TEST(PracticeTest, isPalindromeTest1) {
-//Practice testObj;
-//ASSERT_TRUE(testObj.isPalindrome("racecar"));
-//}
-//
-//TEST(PracticeTest, isPalindromeTest2) {
-//Practice testObj;
-//ASSERT_TRUE(!testObj.isPalindrome("ab"));
-//}
-//
-//TEST(PracticeTest, isPalindromeTest3) {
-//Practice testObj;
-//ASSERT_TRUE(!testObj.isPalindrome("abcdabcd"));
-//}
-//
-//TEST(PracticeTest, isPalindromeTest4) {
-//Practice testObj;
-//ASSERT_TRUE(testObj.isPalindrome("\0\n.abcd1234🥴4321dcba.\n\0"));
-//}
-//
-//
-//TEST(PracticeTest, sortDescendingTest1) {
-//Practice testObj;
-//int a = 1, b = 2, c = 3;
-//testObj.sortDescending(a, b, c);
-//ASSERT_TRUE(a >= b && b >= c);
-//}
-//
-//TEST(PracticeTest, sortDescendingTest2) {
-//Practice testObj;
-//int a = 3, b = 2, c = 1;
-//testObj.sortDescending(a, b, c);
-//ASSERT_TRUE(a >= b && b >= c);
-//}
-//
-//TEST(PracticeTest, sortDescendingTest3) {
-//Practice testObj;
-//int a = 2, b = 3, c = 1;
-//testObj.sortDescending(a, b, c);
-//ASSERT_TRUE(a >= b && b >= c);
-//}
-//
-//TEST(PracticeTest, sortDescendingTest3) {
-//Practice testObj;
-//
-//ASSERT_TRUE(a >= b && b >= c);
-//}
+
+TEST(PracticeTest, SmokeTestAssertTrue)
+{
+ASSERT_TRUE(true);
+}
+
+TEST(PracticeTest, SmokeTestMath)
+{
+
+ASSERT_TRUE(2 > 1);
+}
+
+TEST(PracticeTest, StartingRepeatsCase) {
+Practice testObj;
+ASSERT_EQ(testObj.count_starting_repeats("zzzZzzzzz"), 3);
+}
+
+TEST(PracticeTest, StartingRepeatsOnlyRepeat) {
+Practice testObj;
+ASSERT_EQ(testObj.count_starting_repeats("ppppppppppp"), 11);
+}
+
+TEST(PracticeTest, StartingRepeatsNoRepeat) {
+Practice testObj;
+ASSERT_EQ(testObj.count_starting_repeats(""), 0);
+}
+
+TEST(PracticeTest, StartingRepeatsFourSpace) {
+Practice testObj;
+ASSERT_EQ(testObj.count_starting_repeats("    "), 4);
+}
+
+TEST(PracticeTest, StartingRepeatsEvenNullTerminator) {
+Practice testObj;
+ASSERT_EQ(testObj.count_starting_repeats("\0\0"), 0);
+}
+
+TEST(PracticeTest, StartingRepeatsOddNullTerminator) {
+Practice testObj;
+ASSERT_EQ(testObj.count_starting_repeats("\0"), 0);
+}
+
+TEST(PracticeTest, PalindromeRaceCar) {
+Practice testObj;
+ASSERT_TRUE(testObj.isPalindrome("racecar"));
+}
+
+TEST(PracticeTest, PalindromeNotPalindrome) {
+Practice testObj;
+ASSERT_TRUE(!testObj.isPalindrome("ab"));
+}
+
+TEST(PracticeTest, PalindromeNotPalindromeLong) {
+Practice testObj;
+ASSERT_TRUE(!testObj.isPalindrome("abcdabcd"));
+}
+
+TEST(PracticeTest, PalindromeChaos) {
+Practice testObj;
+ASSERT_TRUE(testObj.isPalindrome("\0\n.abcd1234🥴4321dcba.\n\0"));
+}
+
+TEST(PracticeTest, PalindromeEven) {
+Practice testObj;
+ASSERT_TRUE(testObj.isPalindrome("0000"));
+}
+
+TEST(PracticeTest, PalindromeOdd) {
+Practice testObj;
+ASSERT_TRUE(testObj.isPalindrome("000"));
+}
+
+TEST(PracticeTest, SortDescendingNegative) {
+Practice testObj;
+int a = -1, b = -2, c = -3;
+testObj.sortDescending(a, b, c);
+ASSERT_TRUE(a >= b && b >= c);
+}
+
+TEST(PracticeTest, SortDescendingAscending) {
+Practice testObj;
+int a = 3, b = 2, c = 1;
+testObj.sortDescending(a, b, c);
+ASSERT_TRUE(a >= b && b >= c);
+}
+
+TEST(PracticeTest, SortDescendingRandom) {
+Practice testObj;
+int a = 2, b = 3, c = 1;
+testObj.sortDescending(a, b, c);
+ASSERT_TRUE(a >= b && b >= c);
+}
+
+TEST(PracticeTest, SortDescendingPresortedV1) {
+Practice testObj;
+int a = 1, b = 2, c = 3;
+testObj.sortDescending(a, b, c);
+ASSERT_TRUE(a >= b && b >= c);
+}
+
+TEST(PracticeTest, SortDescendingZeros) {
+Practice testObj;
+int a = 0, b = 0, c = 0;
+testObj.sortDescending(a, b, c);
+ASSERT_TRUE(a == b && b == c);
+}
+
+TEST(PracticeTest, SortDescendingPresortedV2) {
+Practice testObj;
+int a = -10, b = -100, c = -1000;
+testObj.sortDescending(a, b, c);
+ASSERT_TRUE(a >= b && b >= c);
+}
 
 
-TEST(PracticeTest, isAllNighter1) {
+TEST(PracticeTest, AllNighterNormal) {
 Practice testObj;
 int nights[7] = {7, 7, 7, 0, 7, 7, 7};
 int *k = testObj.allnighter(nights);
-printf("%d", *k);
 
 ASSERT_TRUE(*k
 == nights[3]);
 }
 
-TEST(PracticeTest, isAllNighter2) {
+TEST(PracticeTest, AllNighterNullTerminator) {
 Practice testObj;
-int nights[7] = {7, 7, 7, 0, 7, 7, 7};
+int nights[7] = {7, -7, 7, 0, 7, (int)'\0', 7};
 int *k = testObj.allnighter(nights);
-printf("%d", *k);
+
+ASSERT_TRUE(*k == nights[3]);
+}
+
+TEST(PracticeTest, AllNighterOver24Hours) {
+Practice testObj;
+int nights[7] = {9999, 7, 0, 99, 0, 7, 99};
+int *k = testObj.allnighter(nights);
 
 ASSERT_TRUE(*k
-== nights[3]);
+== nights[2]);
+}
+
+TEST(PracticeTest, AllNighterOversizedArray) {
+Practice testObj;
+int nights[8] = {1, 7, 2, 4, 0, 7, 0, 9};
+int *k = testObj.allnighter(nights);
+
+ASSERT_TRUE(*k
+== nights[4]);
+}
+
+TEST(PracticeTest, AllNighterUndersizedArray) {
+Practice testObj;
+int nights[6] = {1, 7, 2, 4, 3, 9};
+int *k = testObj.allnighter(nights);
+
+ASSERT_TRUE(k == nullptr);
+}
+
+TEST(PracticeTest, AllNighterEmpty) {
+Practice testObj;
+int nights[0] = {};
+int *k = testObj.allnighter(nights);
+ASSERT_TRUE(*k == 0);
 }
